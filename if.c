27c6ac4725f85829a5f430ab47a3a9fc55100698
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-20 13:23:16 +0200

    the switch

---
+++ if.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+  int a = 0;
+
+  if (a == 0)
+  {
+    printf("a is equal to 0\n");
+  }
+  else if (a == 1)
+  {
+    printf("a is equal to 1\n");
+  }
+  else
+  {
+    printf("a is greater than 1\n");
+  }
+}
---
+++ switch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+  int a = 5;
+
+  switch(a)
+  {
+    case 0 : printf("a is equal to 0\n");
+             break;
+    case 1 : printf("a is equal to 1\n");
+             break;
+    default: printf("a is greater than 1\n");
+  }
+}
