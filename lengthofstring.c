caf47a9f658eeb78480b128565f965328a0002ab
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-21 14:20:43 +0200

    cast to int

---
+++ lengthofstring.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include <string.h>
+
+void main()
+{
+  char str1[10] = "first";
+  printf("The length of the string '%s' is %d\n", str1, (int)strlen(str1));
+}
---
+++ multipleformats.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// void main()
+// {
+//   int val;
+//   char result[10];
+//   char string[25] = "The first number is 1";
+//
+//   if (scanf(string, "The %s number is %d", result, &val) == 2)
+//   {
+//     printf("String : %s Value : %d\n", result, val);
+//   }
+//   else
+//   {
+//     printf("I couldn't find two values in that string.\n");
+//   }
+// }
