b6f7c427bd49558af0091a7f1100193803e46c88
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-21 16:06:52 +0200

    scans

---
+++ scan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+  char input[256];
+  int age;
+
+  printf("What is your name, user?\n");
+  scanf("%s", input);
+
+  printf("Hello, %s. How old are you?\n", input);
+  scanf("%d", &age);
+
+  printf("Well, %s, you look young for %d\n", input, age);
+}
