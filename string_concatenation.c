2922273d04583741d00f98e5adfca11444c0738d
Tendai Mutunhire <tmutunhire@gmail.com>
2017-02-21 12:20:15 +0200

    the coolest shenanigan

--- string_concatenation.c
+++ string_concatenation.c
@@ -6,5 +6,25 @@ void main()
   char str2[10] = "second";
   char str3[20];
 
-  
+  char *src, *dst;
+  dst = str3;
+  src = str1;
+  // copy over what is in str1 into str3;
+  while(*src != 0)
+  {
+    *dst = *src;
+    dst++;
+    src++;
+  }
+
+  // now copy over what is in str2 into address str3
+  src = str2;
+  while(*src != 0)
+  {
+    *dst = *src;
+    dst++;
+    src++;
+  }
+
+  printf("Joining %s and %s we get %s\n", str1, str2, str3);
 }
